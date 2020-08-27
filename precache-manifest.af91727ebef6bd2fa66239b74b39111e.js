e444d4b1a888cded818ce236ee3658ef3c49fabb
Prashant Bharti <prashantiitv@gmail.com>
2020-08-27 18:19:27 +0530

    Updates

---
+++ precache-manifest.af91727ebef6bd2fa66239b74b39111e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed941f15b2b7e2881e4b6761897ea5eb",
+    "url": "/rizzle_assignment/index.html"
+  },
+  {
+    "revision": "bbca1845369c2e6608c5",
+    "url": "/rizzle_assignment/static/css/main.608504fc.chunk.css"
+  },
+  {
+    "revision": "f6dd5bdeafbebbdd0347",
+    "url": "/rizzle_assignment/static/js/2.37b14681.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/rizzle_assignment/static/js/2.37b14681.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbca1845369c2e6608c5",
+    "url": "/rizzle_assignment/static/js/main.9d90b3f1.chunk.js"
+  },
+  {
+    "revision": "b1c5df382099a8b4e85c",
+    "url": "/rizzle_assignment/static/js/runtime-main.e3e86fb2.js"
+  }
+]);
